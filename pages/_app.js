4f2f6c959abe6566f4e3c0dec06317097002f23b
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-07-22 00:07:18 +0900

    :sparkles: eslint & prettier 설정

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
+/* eslint-disable react/prop-types,react/jsx-props-no-spreading */
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
 
 export default MyApp;
