9dce9eccbdf05788f51f29020c7e1a1236257f3d
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2022-07-22 16:11:26 +0900

    ProgressBar 만들기

--- src/components/layout/quizLayout/progressBar/index.jsx
+++ src/components/layout/quizLayout/progressBar/index.jsx
@@ -1,3 +1,15 @@
-const ProgressBar = () => <div>1</div>;
+import {
+    ContainerStyle,BarStyle
+  } from '@/components/layout/quizLayout/progressBar/styles';
+
+const ProgressBar = () => {
+    return (
+        <ContainerStyle>
+            <BarStyle>
+                10
+            </BarStyle>
+        </ContainerStyle>
+    )
+}
 
 export default ProgressBar;
---
+++ src/components/layout/quizLayout/progressBar/styles.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const ContainerStyle = styled.div`
+    width: 100%;
+    height:4px;
+    margin-top:44px;
+    border-radius:10;
+    background-color: #F2F2F2;
+`;
+
+export const BarStyle = styled.div`
+    width: 50%;
+    height:4px;
+    margin-top:44px;
+    border-radius: 50;
+    background-color: #34AA70;
+`;
